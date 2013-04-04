d132b253cef1cb6ba7e3700624a486174215842d
D&I <steve-india@appacademy.io>
2013-04-04 14:33:48 -0700

    Add #edit and #create views for Band

--- Music/app/controllers/bands_controller.rb
+++ Music/app/controllers/bands_controller.rb
@@ -19,4 +19,20 @@ class BandsController < ApplicationController
 			render :new
 		end
 	end
+
+	def edit
+		@band = Band.find(params[:id])
+	end
+
+	def update
+		@band = Band.find(params[:id])
+	  @band.update_attributes(params[:band])
+		redirect_to band_url(@band)
+	end
+
+	def destroy
+		@band = Band.find(params[:id])
+		@band.destroy
+		redirect_to bands_url
+	end
 end
